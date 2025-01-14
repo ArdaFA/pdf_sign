766fd5c39c97d5287b7021ad641f6422f583982f
Arda Fikret Azakli <ardafikretazakli@gmail.com>
2025-01-14 22:59:19 +0100

    file picking button and function added

--- lib/features/pdf_viewer/pdf_viewer_screen.dart
+++ lib/features/pdf_viewer/pdf_viewer_screen.dart
@@ -1,14 +1,28 @@
 // pdf viewer UI
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:sign_pdf/features/pdf_viewer/pdf_viewer_controller.dart';
 
 class PdfViewerScreen extends StatelessWidget {
+
+  File? pdfFile; // refers to the file that is going to be selected
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('PDF Viewer')),
-      body: Center(
-        child: Text('PDF'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              pdfFile = pickPDFfile() as File?;
+            },
+            child: Text('Pick a File'),
+          ),
+        ],
       ),
     );
   }
